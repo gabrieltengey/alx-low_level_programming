074e23e7e20639fda9627aae7e303a057f2df371
Gabriel Tengey <gabrieltengey123@gmail.com>
2023-02-16 13:01:12 -0800

    print a string to the output using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+/**
+ * main - print string to the standard output
+ *
+ *Description: using the main function,
+ *this program prints "with proper grammar, but the outcome is a piece of art"
+ *to the output
+ *
+ *Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
