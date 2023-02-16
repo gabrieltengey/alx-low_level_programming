808228b390d0cc069c174a6da7dae39bf6010a72
Gabriel Tengey <gabrieltengey123@gmail.com>
2023-02-16 13:54:02 -0800

    print size of data type

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,6 +14,6 @@ int main(void)
 printf("Size of a char: %ld byte(s)\n", sizeof(char));
 printf("Size of an int: %ld byte(s)\n", sizeof(int));
 printf("Size of a long int: %lu byte(s)\n", sizeof(long));
-printf("Size of a long long float: %lu byte(s)\n", sizeof(long long int));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 printf("Size of a float: %ld byte(s)\n", sizeof(float));
 }
