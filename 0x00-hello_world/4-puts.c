8132f71590e650e036d2203249a4c91707e198ff
Gabriel Tengey <gabrieltengey123@gmail.com>
2023-02-16 12:47:36 -0800

    print string using puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+/**
+* main - output a string to the standard output
+* Description: using main function
+* this program prints "Programming is like building a multiligual puzzle
+* Return: 0
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
