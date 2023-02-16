846460379221c7e53ef53a6b39c5df3808106b53
Gabriel Tengey <gabrieltengey123@gmail.com>
2023-02-16 14:36:59 -0800

    print to the stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - print to the standard output
+ *
+ *Description: using the main function,
+ *this program print "and that piece of art is useful"
+ *to the stderr
+ *
+ *Return: 0
+ */
+
+
+int main(void)
+{
+fprintf(stderr, "and that piece of art is useful\" - \
+Dora Korpar, 2015-10-19\n");
+return (0);
+}
