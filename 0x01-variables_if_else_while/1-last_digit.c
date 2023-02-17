03f409ec1ac254466ca07d56b20870d133a7258a
Gabriel Tengey <gabrieltengey123@gmail.com>
2023-02-17 18:09:03 +0000

    check last digit in an int

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+
+/**
+ *main - starting point of c program
+ *
+ *Description: using the main function,
+ *this program print the last digit in a randomly
+ *generated number.
+ *
+ *Return: 0
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	int ld = n % 10;
+	if (ld > 5)
+	{
+	  printf("Last digit of %d is %d and is greater than 5\n", n, ld);
+	} else if ((ld < 6) && (ld != 0))
+	{
+	  printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld);
+	} else
+	  printf("Last digit of %d is %d and is 0", n, ld);
+	return (0);
+}
