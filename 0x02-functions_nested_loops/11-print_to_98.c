f174ba501b734df4a1208ced2d789cac8a684547
Gabriel Tengey <gabrieltengey123@gmail.com>
2023-02-21 19:19:17 +0000

    print number from n t0 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ *print_to_98 - function
+ *
+ *Description: count from n to 98
+ *@n: represent the number
+ *
+ */
+
+void print_to_98(int n)
+{
+	int counter;
+
+	if (n < 98)
+	{
+		for (counter = n; n <= 98; counter++)
+		{
+			_putchar(counter);
+			_putchar(',');
+			_putchar(' ');
+
+		}
+	} else if (n > 98)
+	{
+		for (counter = n; n >= 98; counter--)
+		{
+			_putchar(counter);
+			_putchar(',');
+			_putchar(' ');
+		}
+	} else
+	{
+		_putchar(n);
+	}
+}
