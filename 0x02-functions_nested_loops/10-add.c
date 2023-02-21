5aeb120337104e3b2dc2d983c2a7b6237800706e
Gabriel Tengey <gabrieltengey123@gmail.com>
2023-02-21 19:03:15 +0000

    Sum two integers and return the value

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.c"
+
+/**
+ *add - add two integers and return the sum
+ *
+ *Description: takes two int and add them
+ *
+ *@a: holds first numer
+ *@b: holds seconf number
+ *
+ *Return: 0
+ */
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
