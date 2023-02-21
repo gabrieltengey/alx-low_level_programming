e32ffbda1c8bcf7be40fbc2b2603a2c5abb2002a
Gabriel Tengey <gabrieltengey123@gmail.com>
2023-02-21 13:48:36 +0000

    all functions prototype here

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+/**
+* File: main.h
+* Auth: Gabriel Tengey
+* Desc: Header file containing declaration of all
+* functions used in 0x02-functions_nested_loops
+*/
+
+int _putchar(char c);
+void print_alphabet_x10(void);
+void print_alphabet(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int n);
+int print_last_digit(int num);
+void jack_bauer(void);
+void times_table(void);
+int add(int num_1, int num_2);
+void print_to_98(int num);
+void print_times_table(int n);
+
+
+#endif
