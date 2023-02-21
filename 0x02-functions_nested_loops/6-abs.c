9e171496a77aef41048d6738ea13ade6825ec4e1
Gabriel Tengey <gabrieltengey123@gmail.com>
2023-02-21 17:35:28 +0000

    Return absolute integer of value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *_abs - return absolute integer
+ *of a value
+ *
+ *@n: carries the value
+ *
+ *Return: 0
+ */
+
+int _abs(int n)
+{
+	int result;
+
+	result = abs(n);
+
+	return (n);
+}
