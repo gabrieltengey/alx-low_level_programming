11b261768de2d4a2f91f0d4e37284941fcde05f0
Gabriel Tengey <gabrieltengey123@gmail.com>
2023-02-21 14:00:44 +0000

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_alphabet - start point
+ *
+ *Description: print alphabet
+ *Return : 0
+ */
+
+void print_alphabet(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+
+		_putchar(alpha);
+	}
+
+	_putchar('\n');
+
+}
