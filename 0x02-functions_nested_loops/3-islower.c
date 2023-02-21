7d9e23c1f6829390668e71421e0896c66d38999c
Gabriel Tengey <gabrieltengey123@gmail.com>
2023-02-21 14:36:15 +0000

    check for lowercase alphabets

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ *_islower - check if letter is lowercase or not
+ *
+ *@c: takes value
+ *
+ *Return: 0
+ */
+
+int _islower(int c)
+{
+	int result = islower(c);
+
+	return (result);
+
+}
