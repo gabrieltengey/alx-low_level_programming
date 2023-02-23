c2d7b1e397230cb7489ce3b229087c55698c0fee
Gabriel Tengey <gabrieltengey123@gmail.com>
2023-02-23 11:52:58 +0000

    Print numbers from 0 - 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_numbers - print number from 0 - 9
+ *
+ *Return:
+ */
+
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
