7a53a7a56e1d9808185a55ab4614e760867b74e9
Gabriel Tengey <gabrieltengey123@gmail.com>
2023-02-27 15:38:20 +0000

    Print half of a string, followed by a newline

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *puts_half - print half of a string, followed by a new line
+ *
+ *@str: holds the string
+ *
+ *return: always 0
+ */
+
+void puts_half(char *str)
+{
+	int len, c;
+
+	len = strlen(str);
+
+	if (len % 2 != 0)
+	{
+		for (c = (len - 1) / 2; c <= len; c++)
+		{
+			_putchar(str[c]);
+		}
+	} else
+	{
+		for (c = len / 2; c <= len; c++)
+		{
+			_putchar(str[c]);
+		}
+	}
+	_putchar('\n');
+
+}
