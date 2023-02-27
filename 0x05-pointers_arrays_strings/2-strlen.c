722ad8ab80aa406a4fd6722763477a71cc3c770f
Gabriel Tengey <gabrieltengey123@gmail.com>
2023-02-27 13:36:14 +0000

    return the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *_strlen - return the length of a string
+ *
+ *@s: holds the string
+ *
+ *Return: always 0;
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+
+	return (len);
+
+}
