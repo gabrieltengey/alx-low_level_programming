38257b692c3ea38a65b1d1bd1037dbc447008b38
Gabriel Tengey <gabrieltengey123@gmail.com>
2023-02-27 16:08:19 +0000

    print number of elements in an array of integers, followed by a newline

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -16,7 +16,13 @@ void print_array(int *a, int n)
 
 	for ( i = 0; i <= c; i++)
 	{
-		printf("%d, ", a[i]);
+		if ( i == c)
+		{
+			printf("%d", a[i];
+		} else
+		{
+			printf("%d, ", a[i]);
+		}
 	}
 	printf("\n");
 }
