fe10019505677c75d883e90735653d5329cf13d7
Gabriel Tengey <gabrieltengey123@gmail.com>
2023-03-01 10:21:59 +0000

    More pointers, strings and arrays

---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,22 @@
+[Write a function that concatenates two strings.]
+0-strcat.c
+
+[a function that concatenates two strings.]
+1-strncat.c
+
+[a function that copies a string.]
+2-strncpy.c
+
+[a function that compares two strings.]
+3-strcmp.c
+
+[Write a function that reverses the content of an array of integers.]
+4-rev_array.c
+
+[Write a function that changes all lowercase letters of a string to uppercase.]
+5-string_toupper.c
+
+[Write a function that capitalizes all words of a string.]
+6-cap_string.c
+
+[]
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+int _putchar(char);
+char *_strcat(char *dest, char *src);
+char *_strncpy(char *dest, char *src,int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *str);
+char *cap_string(char *str);
+char *leet(char *s);
+char *rot13(char *s);
+void print_number(int n);
+void rev_string(char *n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_line(char *c, int s, int l);
+void print_butter(char *b, int size);
+
+#endif /* MAIN_H */
