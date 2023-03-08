72f5091003fb0f622f2c51e2aab309751e8baf1b
Gabriel Tengey <gabrieltengey123@gmail.com>
2023-03-08 17:36:26 +0000

    print string followed by a newline

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - print string followed by a newline;
+ *
+ *@s: holds the string to be printed
+ *
+ *Return: always 0
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+
+	_putchar(*s);
+	_puts_recursion(s + 1);
+
+	if (*(s + 1) == '\0')
+	{
+		_putchar('\n');
+	}
+}
